8705296dcf5773cb137443e3349b95c29aed1895
Marjorie Jimenez <majimenezjim@uide.edu.ec>
2026-02-25 11:34:20 -0500

    fix: usar solo URL de produccion en api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-
 const api = axios.create({
-  baseURL: isLocal ? 'http://localhost:5000/api' : 'https://simuvida.uidehub.tech/api'
+  baseURL: 'https://simuvida.uidehub.tech/api'
 });
 
 api.interceptors.request.use(config => {
